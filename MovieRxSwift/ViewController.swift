a0af8e408e0be70d0c481b1dce3f21cd20af3bb9
khoi nguyen <ntkhoiptit@gmail.com>
2017-09-17 00:31:27 +0700

    Initial Commit

---
+++ MovieRxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieRxSwift
+//
+//  Created by Khoi Nguyen on 9/17/17.
+//  Copyright © 2017 Khoi Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
